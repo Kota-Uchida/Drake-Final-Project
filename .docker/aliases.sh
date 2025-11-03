915a5b8b4c871ce02d849b33753033e84c6e4f16
root <root@NSurface.localdomain>
2025-11-02 23:15:21 -0500

    added aliases

---
+++ .docker/aliases.sh
@@ -0,0 +1,7 @@
+# Custom Aliases
+# This aliases are automatically read when the docker is built.
+
+alias cddrake='cd /home/drake'
+alias build='/home/drake/build.sh'
+alias reloadaliases='source /etc/profile.d/aliases.sh'
+
--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker-compose up -d
+set -e
+
+cd "$(dirname "$0")"
+
+docker-compose -f .docker/docker-compose.yaml up -d
 docker exec -it drake bash
